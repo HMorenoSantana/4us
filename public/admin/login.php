e5bfc87b4f432b5bc0e64d0cf3117d7e918877cb
Letícia Magalhães <leticiasilvamagaho@gmail.com>
2025-11-12 20:43:18 -0300

    adicionado link para reconhecimento do estilo.css

--- public/admin/login.php
+++ public/admin/login.php
@@ -21,6 +21,8 @@ if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'POST') {
 <html lang="pt-br">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <link rel="stylesheet" href="<?php URL_BASE ?>assets/css/estilo.css"> -->
+
 <title>Login do Estagiário</title>
 <style>
   body{
